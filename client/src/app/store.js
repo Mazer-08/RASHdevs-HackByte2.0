945e02bf6b8afd68d3bf284b85aa46a2dff847b9
Sabhya <sabhya.1624@gmail.com>
2024-04-06 02:54:49 +0530

    Added auth structure

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import testReducer from "../features/testSlice";
+import authReducer from "../features/authSlice";
 
 export const store = configureStore({
     reducer: {
         test: testReducer,
+        auth: authReducer,
     },
 });
