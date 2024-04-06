52600afcbe753fdf347e34b7743ca491758dc982
Sabhya <sabhya.1624@gmail.com>
2024-04-06 21:06:25 +0530

    Added redirecting functionality on signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -20,17 +20,22 @@ const Signup = () => {
     const navigate = useNavigate();
     const registerStage = useSelector((state) => state.auth.registerStage);
 
+    const token = localStorage.getItem("token");
+
   return (
-    <div className='h-screen w-full flex justify-center items-center'>
-        {(registerStage === 0) ? <SignupLogin/> :
-         (registerStage === 1) ? <Register02/> :
-         (registerStage === 2) ? <Register03/> :
-         (registerStage === 3) ? <Register04/> :
-         (registerStage === 4) ? <Register05/> :
-         (registerStage === 5) ? <Register06/> :
-         (registerStage === 6) ? <Register07/> :
-         navigate('/') }
-    </div>
+    <>
+      {token && navigate('/')}
+      <div className='h-screen w-full flex justify-center items-center'>
+          {(registerStage === 0) ? <SignupLogin/> :
+          (registerStage === 1) ? <Register02/> :
+          (registerStage === 2) ? <Register03/> :
+          (registerStage === 3) ? <Register04/> :
+          (registerStage === 4) ? <Register05/> :
+          (registerStage === 5) ? <Register06/> :
+          (registerStage === 6) ? <Register07/> :
+          navigate('/') }
+      </div>
+    </>
   )
 }
 
